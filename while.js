e828879a8d6f22c5cedda783cce8f59f872a8aae
asad <asaduzzamanprogrammer@gmail.com>
2024-02-07 20:13:25 +0600

    chinta hoy take niye

--- while.js
+++ while.js
@@ -4,3 +4,16 @@ if (number > 9){
 }
 
 console.log("fuck you ugly bitch");
+
+// let num = 0;
+
+// while(num < 5){
+//     console.log("fucking", num);
+//     num++;
+// }
+
+let num = 0;
+while(num < 101){
+    console.log("তোমাকে নিয়ে অনেক চিন্তা হয় 😭😭😭", num);
+    num++;
+}
