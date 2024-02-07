23a2c4ce7a917be1c753cda8bcd7ebcdf9ab4d5a
asad <asaduzzamanprogrammer@gmail.com>
2024-02-07 19:43:51 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,12 @@
+// const number = [12, 21, 32, 56];
+// console.log(number);
+// for(const num of number){
+//     console.log(num)
+// }
+
+const fruite = ['orange', 'colaps', 'start', 'end'];
+
+for (const fruit of fruite){
+    console.log(fruit)
+    console.log("i want to eat")
+}
