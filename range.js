86c0458f3a056afab5cf75ffd999dc67d41700b5
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 09:56:51 +0600

    foor-sum

--- range.js
+++ range.js
@@ -7,11 +7,11 @@
 //     num++
 // }
 
-let num = 1;
-let sum = 0;
-while(num < 10){
-    console.log(num);
-    sum = sum + num;
-    console.log("sum :", sum);
-    num++;
-}
+// let num = 1;
+// let sum = 0;
+// while(num < 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log("sum :", sum);
+//     num++;
+// }
