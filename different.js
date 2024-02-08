a5e8351e16244c0b236f4c3cc140c22dc83209b4
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 10:13:11 +0600

    different-js-2

---
+++ different.js
@@ -0,0 +1,24 @@
+// for (let i = 0; i < 20; i++){
+//     if(i%2===1){
+//         console.log("bijor", i)
+//     }
+//     else(
+//         console.log("jor", i)
+//     )
+// }
+
+// for (i = 0; i <= 30; i++){
+//     // console.log(i)
+//     if(i % 5 ===0 || i % 3 ===0){
+//         console.log(i)
+//     }
+// }
+// let sum = 0;
+// for (i = 0; i <= 30; i++){
+//     // console.log(i)
+//     if(i % 5 ===0){
+//         // console.log(i)
+//         sum = sum + i;
+//         console.log(sum)
+//     }
+// }
