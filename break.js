7d19a4489cf69838ebf0907a1ed95655cde005e1
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 10:24:30 +0600

    break

---
+++ break.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+//     if ( i >= 5){
+//         break;
+//     }
+// }
+
+// let i = 54;
+
+// while(i > 25 ){
+//     if( i < 50){
+//         break;
+//     }
+//     console.log(i);
+//     i--;
+// }
