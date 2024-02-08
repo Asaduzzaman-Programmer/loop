383b6fe8753f89d0eb6cc7367825b95c02fdf824
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 09:54:58 +0600

    for-sum

---
+++ for-sum.js
@@ -0,0 +1,31 @@
+// let sum = 0;
+
+// for (let i = 5; i <= 10; i++){
+//     sum = sum + i;
+//     console.log(i);
+// }
+
+// console.log(sum);
+
+// for(let main = 200; main <= 250: main++){
+//     console.log(main)
+// }
+
+// let sum = 0;
+
+// for (let i = 200; i <= 250; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
